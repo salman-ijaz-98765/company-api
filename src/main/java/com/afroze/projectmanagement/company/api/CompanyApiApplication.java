070862673fa9160e787f143b5aee37ce01b4bb91
afroze9 <afrozeamjad@gmail.com>
2023-02-13 19:15:25 +0500

    updated eureka config for aws

--- src/main/java/com/afroze/projectmanagement/company/api/CompanyApiApplication.java
+++ src/main/java/com/afroze/projectmanagement/company/api/CompanyApiApplication.java
@@ -4,9 +4,14 @@ import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -24,5 +29,21 @@ public class CompanyApiApplication {
 	@Bean
 	Logger.Level feignLoggerLevel() {
 		return Logger.Level.FULL;
+	}
+
+	@Bean
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+		String ip = null;
+		try {
+			ip = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+		config.setIpAddress(ip);
+		config.setPreferIpAddress(true);
+
+		return config;
 	}
 }
